394112c777da54295dde073c3e994af4ae9a20ae
张鹏 <scbeta@qq.com>
2023-07-12 18:16:25 +0800

    初步可以推送RTP流。

---
+++ src/Hikvision.ISUPSDK.Api/Rtp/RtpSenderOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hikvision.ISUPSDK.Api
+{
+    public class RtpSenderOptions
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public uint SSRC { get; set; }
+    }
+}
